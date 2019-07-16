4f3b0f57e41f5a6f0b2bcad8810ce6e8c62e9a1c
iceGogh <4535292@qq.com>
2019-07-16 14:43:00 +0800

    #ui# loading, button

--- components/g-params.vue
+++ components/g-params.vue
@@ -1,56 +1,73 @@
 <template>
   <div id="params">
-    <div class="url-area">
-      <div class="method">{{methodType}}</div>
-      <input type="text" v-model="queryUrl" >
-      <div class="send" @click="sendFn">
-        <span>发送</span>
-        <!-- <i class="ns-font ns-tubiaozhizuomoban"></i> -->
-      </div>
-    </div>
-    <div class="option-area">
-      <div class="title">
-        <span :class="{selected: headerSelectStatus }" @click="selectOpt('headerSelectStatus')">Header</span>
-        <span :class="{selected: bodySelectStatus }" @click="selectOpt('bodySelectStatus')">Body</span>
-        <div class="add-btn">
-          <span v-if="headerSelectStatus" @click="addOpt('headersArr')">add header</span>
-          <span v-if="bodySelectStatus" @click="addOpt('paramsArr')">add body</span>
+    <a-spin :spinning="requestStatus" tip="Requesting...">
+      <div class="url-area">
+        <div class="method">
+          <a-dropdown>
+            <a-menu slot="overlay" @click="handleMenuClick">
+              <a-menu-item key="GET">GET</a-menu-item>
+              <a-menu-item key="POST">POST</a-menu-item>
+            </a-menu>
+            <a-button style="margin-left: 8px">
+              {{methodType}}
+              <a-icon type="down" />
+            </a-button>
+          </a-dropdown>
+        </div>
+        <input type="text" v-model="queryUrl" />
+        <div class="send" @click="sendFn">
+          <span>
+            <a-button type="primary" :loading="requestStatus">发送</a-button>
+          </span>
         </div>
       </div>
-      <div class="param">
-        <div class="item clearfix" v-for="(item,key) in headersArr" :key="'h' + key">
-          <i class="ns-font ns-require-true" :class="{ opacity0: !item.require }"></i>
-          <i class="ns-font ns-wenhao" @click="showName(item.comment)"></i>
-          <span class="type">{{item.type}}</span>
-          <div class="keyin">
-            <span class="key">Key</span>
-            <input type="text" v-model="item.name">
-            <span class="value">Value</span>
-            <input type="text" v-model="item.default">
+      <div class="option-area">
+        <div class="title">
+          <span
+            :class="{selected: headerSelectStatus }"
+            @click="selectOpt('headerSelectStatus')"
+          >Header</span>
+          <span :class="{selected: bodySelectStatus }" @click="selectOpt('bodySelectStatus')">Body</span>
+          <div class="add-btn">
+            <span v-if="headerSelectStatus" @click="addOpt('headersArr')">add header</span>
+            <span v-if="bodySelectStatus" @click="addOpt('paramsArr')">add body</span>
           </div>
         </div>
-        <hr>
-        <div class="item clearfix" v-for="(item,key) in paramsArr" :key="'p' + key">
-          <i class="ns-font ns-require-true" :class="{ opacity0: !item.require }"></i>
-          <i class="ns-font ns-wenhao" @click="showName(item.comment)"></i>
-          <span class="type">{{item.type}}</span>
-          <div class="keyin">
-            <span class="key">Key</span>
-            <input type="text" v-model="item.name">
-            <span class="value">Value</span>
-            <input type="text" v-model="item.default">
+        <div class="param">
+          <div class="item clearfix" v-for="(item,key) in headersArr" :key="'h' + key">
+            <i class="ns-font ns-require-true" :class="{ opacity0: !item.require }"></i>
+            <i class="ns-font ns-wenhao" @click="showName(item.comment)"></i>
+            <span class="type">{{item.type}}</span>
+            <div class="keyin">
+              <span class="key">Key</span>
+              <input type="text" v-model="item.name" />
+              <span class="value">Value</span>
+              <input type="text" v-model="item.default" />
+            </div>
+          </div>
+          <hr />
+          <div class="item clearfix" v-for="(item,key) in paramsArr" :key="'p' + key">
+            <i class="ns-font ns-require-true" :class="{ opacity0: !item.require }"></i>
+            <i class="ns-font ns-wenhao" @click="showName(item.comment)"></i>
+            <span class="type">{{item.type}}</span>
+            <div class="keyin">
+              <span class="key">Key</span>
+              <input type="text" v-model="item.name" />
+              <span class="value">Value</span>
+              <input type="text" v-model="item.default" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="response-area">
-      <div class="title">
-        <span>Response</span>
+      <div class="response-area">
+        <div class="title">
+          <span>Response</span>
+        </div>
+        <div class="response-data">
+          <pre v-if="responseData">{{responseData}}</pre>
+        </div>
       </div>
-      <div class="response-data">
-        <pre v-if="responseData">{{responseData}}</pre>
-      </div>
-    </div>
+    </a-spin>
   </div>
 </template>
 
@@ -59,8 +76,9 @@ import { rewrite_api } from '~/api/methods'
 export default {
   data() {
     return {
-      // queryUrl: 'http://127.0.0.2/ajax.php',
-      queryUrl: 'http://localhost:9998',
+      queryUrl: 'http://127.0.0.2/ajax.php',
+      // queryUrl: 'http://localhost:9998',
+      requestStatus: false,
       methodType: 'POST',
       headerSelectStatus: false,
       bodySelectStatus: true,
@@ -75,40 +93,42 @@ export default {
       responseData: ''
     }
   },
-  beforeMount() {
-  },
+  beforeMount() {},
   watch: {
     api(v) {
       if (v) {
       }
     }
   },
-  mounted() {
-  },
-  beforeDestroy() {
-  },
+  mounted() {},
+  beforeDestroy() {},
   methods: {
     clearFn() {
       this.paramsArr = []
       this.responseData = ''
     },
+    handleMenuClick(e) {
+      this.methodType = e['key']
+    },
     addOpt(arr) {
-      this[arr] = this[arr].concat([{
-        comment: '',
-        default: '',
-        name: '',
-        require: false,
-        type: 'string'
-      }])
+      this[arr] = this[arr].concat([
+        {
+          comment: '',
+          default: '',
+          name: '',
+          require: false,
+          type: 'string'
+        }
+      ])
     },
     selectOpt(type) {
       this[type] = !this[type]
     },
-    queryCmd(api) {
-    },
+    queryCmd(api) {},
     sendFn() {
       const headers = {}
       const params = {}
+      this.requestStatus = true
       this.headersArr.forEach(e => {
         if (e['name']) {
           Object.assign(headers, {
@@ -124,14 +144,19 @@ export default {
         }
       })
       const d = JSON.stringify({
-        headers, params
+        headers,
+        params
       })
-      rewrite_api(this.queryUrl, d, this.methodType).then(s => {
-        this.responseData = s
-      }).catch(e => {})
+      rewrite_api(this.queryUrl, d, this.methodType)
+        .then(s => {
+          this.requestStatus = false
+          this.responseData = s
+        })
+        .catch(e => {
+          this.requestStatus = false
+        })
     },
-    showName(message) {
-    }
+    showName(message) {}
   }
 }
 </script>
@@ -154,6 +179,7 @@ export default {
   padding: 40px;
   overflow-y: auto;
   box-sizing: border-box;
+  .ant-spin-container {
   > div {
     background: rgba(255, 255, 255, 1);
     box-shadow: 0px 3px 15px 0px rgba(194, 196, 237, 0.4);
@@ -163,7 +189,7 @@ export default {
     height: 40px;
     margin-bottom: 40px;
     .method {
-      width: 80px;
+      width: 100px;
       height: 40px;
       line-height: 40px;
       color: #cdca32;
@@ -173,11 +199,12 @@ export default {
     input {
       display: block;
       float: left;
-      width: calc(100% - 180px);
+      width: calc(100% - 200px);
       height: 40px;
       line-height: 40px;
       color: #666;
       border: 0;
+      text-indent: 10px;
     }
     .send {
       float: right;
@@ -185,21 +212,24 @@ export default {
       height: 40px;
       line-height: 40px;
       text-align: center;
-      background: #409eff;
+      // background: #409eff;
       color: #fff;
       font-size: 14px;
       cursor: pointer;
       span {
+        position: relative;
         display: inline-block;
         width: 100%;
         height: 40px;
         line-height: 40px;
-      }
-      i {
-        display: inline-block;
-        height: 40px;
-        line-height: 40px;
-        color: rgb(149, 208, 212);
+        button {
+          position: absolute;
+          left: 0;
+          top: 0;
+          width: 100%;
+          height: 40px;
+          line-height: 40px;
+        }
       }
     }
   }
@@ -303,5 +333,7 @@ export default {
       font-size: 14px;
     }
   }
+  }
+
 }
 </style>
