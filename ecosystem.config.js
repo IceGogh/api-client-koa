a1ecf6e42dd7acf50a12e711fe86d20a0d2cede6
iceGogh <4535292@qq.com>
2019-07-17 15:11:24 +0800

    #debug# 127 拦截、err拦截、 pm2 instances

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,11 +3,13 @@ module.exports = {
     {
       name: 'dev-mode',
       script: './server/index.js',
+      watch: true,
       env: {
         'NODE_ENV': 'development'
       }
     }, {
       name: 'pro-mode',
+      instances: 2,
       script: './server/index.js',
       env: {
         'NODE_ENV': 'production'
